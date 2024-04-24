60f6d9ea2ccdaaaf90f5efa88196f347940faf66
BRAZILIANiptv <131699675+BRAZILIANiptv@users.noreply.github.com>
2024-04-24 13:31:41 -0300

    Update time.py

--- time.py
+++ time.py
@@ -1,2 +1,13 @@
-name: TIME TEST
-run: timeout-minutes: 1000000
+pytime.timestamps()
+
+pytime.microtime()
+
+pytime.sleep(second: 9999999999999999999999999999999999)
+
+import time
+
+seconds = 998898998898988898898988998988988.989888988988888888
+
+local_time = time.ctime(seconds)
+
+print("Local time:", local_time)
